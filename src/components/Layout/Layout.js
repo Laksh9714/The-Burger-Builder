5e7cba3fc28435fa08bd152fd9c4bbc6e6c03ade
Lakshmeesha Shetty <lss180005@utdallas.edu>
2020-12-29 22:59:49 -0600

    Sidedrawer added but issues with mobile interface

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Aux from '../../hoc/Auxiliary';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
+import Sidedrawer from '../Navigation/Sidedrawer/Sidedrawer';
 
 import classes from './Layout.css';
 
 const layout = (props) =>(
     <Aux>
         <Toolbar/>
+        <Sidedrawer/>
         <main className = {classes.Content}>
             {props.children}
         </main>
