497dee5554a8094fa01de260a28d8715a4b73a4a
Lakshmeesha Shetty <lss180005@utdallas.edu>
2021-01-01 02:00:21 -0600

    posting of data successful to firebase

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -5,6 +5,8 @@ import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
 
+import axios from '../../axios-orders';
+
 const INGREDIENT_PRICES = {
     salad : 0.5,
     cheese : 0.4,
@@ -53,8 +55,29 @@ class BurgerBuilder extends Component{
     }
 
     purchaseContinueHandler = () =>{
-        alert("You continue!!");
-    }
+        const order = {
+            ingredients : this.state.ingredients,
+            price : this.state.totalprice,
+            customer : {
+                name : "Laksh",
+                address : {
+                    street : "Shiv Mandir Road",
+                    zipcode : "421201",
+                    country : "India"
+                },
+                email : "lsss@gmail.com"
+            },
+            deliveryMethod : "fastest"
+        }
+
+        axios.post('/orders.json',order)
+            .then(response => {
+                console.log(response);
+            }) 
+            .catch(error => {
+                console.log(error);
+            });
+    } 
 
     addIngredientHandler = (type) => {
         const oldCount = this.state.ingredients[type];
