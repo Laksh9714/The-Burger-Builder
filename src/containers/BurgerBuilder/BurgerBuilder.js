d5d423fb9dabe7242d5de5f3f590a8cc2f08854a
Lakshmeesha Shetty <lss180005@utdallas.edu>
2021-01-27 15:11:27 -0600

    Setting up link change on continue click

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -65,31 +65,32 @@ class BurgerBuilder extends Component{
 
     purchaseContinueHandler = () =>{
 
-        this.setState({loading:true});
+        // this.setState({loading:true});
 
 
-        const order = {
-            ingredients : this.state.ingredients,
-            price : this.state.totalprice,
-            customer : {
-                name : "Laksh",
-                address : {
-                    street : "Shiv Mandir Road",
-                    zipcode : "421201",
-                    country : "India"
-                },
-                email : "lsss@gmail.com"
-            },
-            deliveryMethod : "fastest"
-        }
+        // const order = {
+        //     ingredients : this.state.ingredients,
+        //     price : this.state.totalprice,
+        //     customer : {
+        //         name : "Laksh",
+        //         address : {
+        //             street : "Shiv Mandir Road",
+        //             zipcode : "421201",
+        //             country : "India"
+        //         },
+        //         email : "lsss@gmail.com"
+        //     },
+        //     deliveryMethod : "fastest"
+        // }
 
-        axios.post('/orders.json', order)
-            .then(response => {
-                this.setState({loading:false , purchasing : false} );
-            }) 
-            .catch(error => {
-                this.setState({loading:false , purchasing : false});
-            });
+        // axios.post('/orders.json', order)
+        //     .then(response => {
+        //         this.setState({loading:false , purchasing : false} );
+        //     }) 
+        //     .catch(error => {
+        //         this.setState({loading:false , purchasing : false});
+        //     });
+        this.props.history.push('/checkout');
     } 
 
     addIngredientHandler = (type) => {
