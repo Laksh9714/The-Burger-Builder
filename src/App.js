2eccd295e90bd6976be1e087f55a8816001ac725
Lakshmeesha Shetty <lss180005@utdallas.edu>
2021-01-27 17:02:49 -0600

    Adding my orders page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React,{Component} from 'react';
+import {Route,Switch}  from 'react-router-dom';
+
+
 import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import Checkout from './containers/Checkout/Checkout';
-import {Route,Switch}  from 'react-router-dom';
+import Orders from './containers/Orders/Orders';
+
 
 
 class App extends Component{
@@ -14,6 +18,7 @@ class App extends Component{
         <Layout>
           <Switch>
             <Route path = "/checkout" component = {Checkout}/>
+            <Route path = "/orders" component = {Orders}/>
             <Route path = "/" exact component = {BurgerBuilder}/>
           </Switch>
         </Layout>
