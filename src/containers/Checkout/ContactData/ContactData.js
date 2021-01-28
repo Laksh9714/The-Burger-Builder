8f5544a98b31c8f8c58ce1e4126b7c421afba88b
Lakshmeesha Shetty <lss180005@utdallas.edu>
2021-01-28 16:37:50 -0600

    Added form validation

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -16,7 +16,11 @@ class ContactData extends Component{
                     type : 'text',
                     placeholder : 'Your name'
                 },
-                value : ""
+                value : "",
+                validation : {
+                    required : true
+                },
+                valid : false
             },
             street : {
                 elementType : 'input',
@@ -24,7 +28,11 @@ class ContactData extends Component{
                     type : 'text',
                     placeholder : 'Street'
                 },
-                value : ""
+                value : "",
+                validation : {
+                    required : true
+                },
+                valid : false
             },
             zipcode : {
                 elementType : 'input',
@@ -32,7 +40,13 @@ class ContactData extends Component{
                     type : 'text',
                     placeholder : 'Zipcode'
                 },
-                value : ""
+                value : "",
+                validation : {
+                    required : true,
+                    minLength : 5,
+                    maxLength : 5
+                },
+                valid : false
             },
             country : {
                 elementType : 'input',
@@ -40,7 +54,11 @@ class ContactData extends Component{
                     type : 'text',
                     placeholder : 'Country'
                 },
-                value : ""
+                value : "",
+                validation : {
+                    required : true
+                },
+                valid : false
             },
             email : {
                 elementType : 'input',
@@ -48,7 +66,11 @@ class ContactData extends Component{
                     type : 'email',
                     placeholder : 'Your email'
                 },
-                value : ""
+                value : "",
+                validation : {
+                    required : true
+                },
+                valid : false
             },
             deliveryMethod : {
                 elementType : 'select',
@@ -95,6 +117,25 @@ class ContactData extends Component{
 
     }
 
+    checkValidation(value,rules){
+        let isValid = false;
+
+        
+        if(rules.required){
+            isValid = value.trim() !== ""
+        }
+
+        if(rules.minLength){
+            isValid = value.length >= rules.minLength
+        }
+
+        if(rules.maxLength){
+            isValid = value.length <= rules.maxLength
+        }
+
+        return isValid;
+    }
+
 
     inputChangedHandler = (event, inputIdentifier) => {
         const updatedOrderForm = {
@@ -104,7 +145,9 @@ class ContactData extends Component{
            ...updatedOrderForm[inputIdentifier]
         }
         updatedFormElement.value = event.target.value;
+        updatedFormElement.valid = this.checkValidation(updatedFormElement.value, updatedFormElement.validation);
         updatedOrderForm[inputIdentifier] = updatedFormElement;
+        console.log(updatedFormElement);
         this.setState({orderForm : updatedOrderForm});
     }
 
