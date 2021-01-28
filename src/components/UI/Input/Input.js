351e642c7b36c0b11e3c9539c22825b5842b13d3
Lakshmeesha Shetty <lss180005@utdallas.edu>
2021-01-28 16:08:54 -0600

    Added onChangeHandler's

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -12,21 +12,24 @@ const input = (props) => {
             inputElement = <input 
                 className = {classes.inputElement} 
                 {...props.elementConfig} 
-                value = {props.value}/>;
+                value = {props.value}
+                onChange = {props.changed}/>;
             break;
 
         case ('textarea'):
             inputElement = <textarea 
                 className = {classes.inputElement} 
                 {...props.elementConfig} 
-                value = {props.value}/>;
+                value = {props.value}
+                onChange = {props.changed}/>;
             break;
 
         case ('select'):
             inputElement = <select 
                 className = {classes.inputElement} 
                 {...props.elementConfig} 
-                value = {props.value}>
+                value = {props.value}
+                onChange = {props.changed}>
                 {props.elementConfig.options.map(option =>(
                     <option value = {option.value}
                         key = {option.value}>{option.displayValue}</option>
@@ -38,7 +41,8 @@ const input = (props) => {
             inputElement = <input 
                 className = {classes.inputElement} 
                 {...props.elementConfig} 
-                value = {props.value}/>;
+                value = {props.value}
+                onChange = {props.changed}/>;
         
     }
 
