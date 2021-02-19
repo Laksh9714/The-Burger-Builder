82b336cb9bfc3c65eef3cfc1b4ce2514b14e196a
Lakshmeesha Shetty <lss180005@utdallas.edu>
2021-02-18 23:43:54 -0600

    Clearing some bugs to fix redirect on order placement

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -50,6 +50,7 @@ class BurgerBuilder extends Component{
     }
 
     purchaseContinueHandler = () =>{
+        this.props.onInitPurchase();
         this.props.history.push('/checkout');
     } 
 
@@ -117,7 +118,8 @@ const mapDispatchToProps = dispatch => {
     return {
         onIngredientAdded : (ingName) => dispatch(burgerBuilderActions.addIngredient(ingName)),
         onIngredientRemoved : (ingName) => dispatch(burgerBuilderActions.removeIngredient(ingName)),
-        onInitIngredients : () => dispatch(burgerBuilderActions.initIngredients())
+        onInitIngredients : () => dispatch(burgerBuilderActions.initIngredients()),
+        onInitPurchase : () => dispatch(burgerBuilderActions.purchaseInit())
     }
 }
 
