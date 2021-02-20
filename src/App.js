e03bf162a99b4d354ac0980c6f94204e3fd6a166
Lakshmeesha Shetty <lss180005@utdallas.edu>
2021-02-19 20:45:12 -0600

    Adding authentication and sign up complete

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import Checkout from './containers/Checkout/Checkout';
 import Orders from './containers/Orders/Orders';
+import Auth from './containers/Auth/Auth';
 
 
 
@@ -20,6 +21,7 @@ class App extends Component{
             <Route path = "/checkout" component = {Checkout}/>
             <Route path = "/orders" component = {Orders}/>
             <Route path = "/" exact component = {BurgerBuilder}/>
+            <Route path = "/auth" exact component = {Auth}/>
           </Switch>
         </Layout>
       </div>
