5bb7c1f24b6a4f0582f3fcfbd35cc0d2a0275f78
Lakshmeesha Shetty <lss180005@utdallas.edu>
2021-02-24 12:47:48 -0600

    Repaired some small bugs

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React,{Component} from 'react';
-import {Route,Switch , withRouter}  from 'react-router-dom';
+import {Route,Switch , withRouter, Redirect}  from 'react-router-dom';
 import {connect} from 'react-redux';
 
 
@@ -20,19 +20,42 @@ class App extends Component{
   }
 
   render(){
+
+    let routes = (
+      <Switch>
+            <Route path = "/auth" exact component = {Auth}/>
+            <Route path = "/" exact component = {BurgerBuilder}/>
+            <Redirect to = "/"/>
+      </Switch>
+
+    );
+
+    if(this.props.isAuthenticated){
+      routes = (
+        <Switch>
+          <Route path = "/logout" exact component = {Logout}/>
+          <Route path = "/auth" exact component = {Auth}/>
+          <Route path = "/checkout" component = {Checkout}/>
+          <Route path = "/orders" component = {Orders}/>
+          <Route path = "/" exact component = {BurgerBuilder}/>
+          <Redirect to = "/" />
+        </Switch>
+      );
+    }
+
     return (
       <div >
         <Layout>
-          <Switch>
-            <Route path = "/checkout" component = {Checkout}/>
-            <Route path = "/orders" component = {Orders}/>
-            <Route path = "/" exact component = {BurgerBuilder}/>
-            <Route path = "/auth" exact component = {Auth}/>
-            <Route path = "/logout" exact component = {Logout}/>
-          </Switch>
+          {routes}
         </Layout>
       </div>
     );
+  }
+}
+
+const mapStateToProps = state => {
+  return {
+    isAuthenticated : state.auth.token!==null
   }
 }
 
@@ -43,4 +66,4 @@ const mapDispatchToProps = dispatch => {
 }
 
 
-export default withRouter(connect(null, mapDispatchToProps)(App));
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(App));
--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -8,9 +8,10 @@ import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
 import * as action from '../../store/actions/index';
 import Spinner from '../../components/UI/Spinner/Spinner';
 
+
 class Orders extends Component{
     componentDidMount(){
-        this.props.onFetchOrders(this.props.token);
+        this.props.onFetchOrders(this.props.token,this.props.userId);
     }
 
     render(){
@@ -39,13 +40,14 @@ const mapStateToProps = state => {
     return {
         orders : state.order.orders,
         loading : state.order.loading,
-        token : state.auth.token
+        token : state.auth.token,
+        userId : state.auth.userId
     }
 }
 
 const mapDispatchToProps = dispatch => {
     return{
-        onFetchOrders : (token) => dispatch(action.fetchOrders(token))
+        onFetchOrders : (token,userId) => dispatch(action.fetchOrders(token,userId))
     }
 }
 
